c93c8a9ed093cafd9bde6bb815d139b3e90ab98b
GitPebble <d.shaburov@inbox.ru>
2023-12-01 21:17:21 +0300

    edited videos

--- app/endpoints/video.py
+++ app/endpoints/video.py
@@ -1,6 +1,7 @@
 import logging
 
 from fastapi import APIRouter, HTTPException
+from pathlib import Path
 from fastapi import Header
 from fastapi.responses import StreamingResponse, FileResponse
 from fastapi.templating import Jinja2Templates
@@ -13,11 +14,12 @@ video_router = APIRouter(
 
 
 @video_router.get(path='/video/{video_id}',
-               summary='Стриминг видео')
+               summary='Стриминг видео', response_class=FileResponse)
 async def video_endpoint(video_id: int):
     try:
-        video_path = f'app/static/test_video{video_id}.mp4'
-        return FileResponse(path=video_path, media_type="video/mp4", )
+        video_path = Path(f'app/static/test_video{video_id}.mp4')
+
+        return FileResponse(path=video_path, media_type="video/mp4")
     except FileNotFoundError:
         raise HTTPException(status_code=418, detail={
             "status": "Error",
